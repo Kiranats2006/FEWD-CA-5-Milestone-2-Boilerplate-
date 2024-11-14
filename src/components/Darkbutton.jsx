68f8d98be3e3ff2021866dd9d629b5e34163950d
Kirana Trupthi Shree <kiranats2006@gmail.com>
2024-11-14 14:48:57 +0530

    All test cases passed

--- src/components/Darkbutton.jsx
+++ src/components/Darkbutton.jsx
@@ -7,6 +7,15 @@ function Darkbutton() {
   // TASK 4: Implement the logic for changing the background and text color of the entire page.
   const toggleDarkMode = () => {
     // When dark mode is active, change background to black and text to white.
+    setIsDarkMode((prevMode)=>!prevMode);
+    if(isDarkMode){
+      document.body.style.background='rgb(240, 240, 240)';
+      document.body.style.color='rgb(0, 0, 0)';
+    }
+    else{
+      document.body.style.background='rgb(24, 24, 24)';
+      document.body.style.color='rgb(240, 240, 240)';
+    }
     // When light mode is active, change background to white and text to black.
   };
 
